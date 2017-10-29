e5cab4cb5c4edafd4ac32b600015d078c6f96230
Emiel Kwakkel <mail@emielkwakkel.nl>
2017-10-29 23:01:01 +0100

    💬 Add Conference Open intent.

---
+++ src/intents/conference-open.intent.js
@@ -0,0 +1,28 @@
+export function conferenceOpen() {
+  const speakersIntroduction = getSpeakersIntroduction();
+  const pronounceSogeti = `I'm far from replacing you Remco. I still have a hard time pronouncing Sogeti. Sogeti it is right?`;
+  const speechOutput = `${pronounceSogeti}. Today we have a very interesting lineup arranged for you. ${speakersIntroduction}`;
+
+  this.emit(":ask", speechOutput, repromtSpeech);
+}
+
+function getSpeakersIntroduction() {
+  const speakers = [
+    {
+      name: 'Emiel',
+      talk: 'Introduce you to developing a skill for Amazon Alexa.'
+    },
+    {
+      name: 'Chris den Arend',
+      talk: 'Show you the Microsoft Bot Framework.'
+    }
+  ];
+
+  let speakersIntroduction = 'Let me anounce to you: ';
+
+  speakers.forEach(speaker => {
+    `${speakersIntroduction} ${name} is going to ${talk} `;
+  });
+
+  return speakersIntroduction;
+}
