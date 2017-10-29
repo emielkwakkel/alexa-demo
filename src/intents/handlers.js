b9c70ed0aa67c8fee6982f5b953cd1d538613b8f
Emiel Kwakkel <mail@emielkwakkel.nl>
2017-10-29 23:26:21 +0100

    ✅ Fix: add missing conferenceOpen import

--- src/intents/handlers.js
+++ src/intents/handlers.js
@@ -1,5 +1,6 @@
 import { bookARoom } from './book-a-room.intent';
 import { countryCheck } from './country-check.intent';
+import { conferenceOpen } from './conference-open.intent';
 import { employeeStatistics } from './employee-statistics.intent';
 import { holidayRemaining } from './holiday-remaining.intent';
 import { mealsOverview } from './meals-overview.intent';
@@ -7,12 +8,12 @@ import { launchRequest } from './launch-request.intent';
 import { unhandled } from './unhandled.intent';
 
 export const handlers = {
-    'BookARoom': bookARoom,
-    'CountryCheck': countryCheck,
-    'ConferenceOpen': conferenceOpen,
-    'HolidayRemaining': holidayRemaining,
-    'EmployeeStatistics': employeeStatistics,
-    'MealsOverview': mealsOverview,
-    'Unhandled': unhandled,
-    'LaunchRequest': launchRequest
+  BookARoom: bookARoom,
+  CountryCheck: countryCheck,
+  ConferenceOpen: conferenceOpen,
+  HolidayRemaining: holidayRemaining,
+  EmployeeStatistics: employeeStatistics,
+  MealsOverview: mealsOverview,
+  Unhandled: unhandled,
+  LaunchRequest: launchRequest,
 };
