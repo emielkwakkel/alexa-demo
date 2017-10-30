7f4ebadb73295ec44eabdda193c747fc46e70ff5
Emiel Kwakkel <mail@emielkwakkel.nl>
2017-10-30 20:45:54 +0100

    🔧 Only generate minified file

--- webpack.config.js
+++ webpack.config.js
@@ -4,37 +4,35 @@ const fs = require('fs');
 const webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        'index': [ 'babel-polyfill', './src/index.js' ],
-        'index.min': [ 'babel-polyfill', './src/index.js' ]
-    },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        library: '[name]',
-        libraryTarget: 'commonjs2',
-        filename: '[name].js'
-    },
-    target: 'node',
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015'],
-                    plugins: ['syntax-flow', 'transform-flow-strip-types']
-                }
-            },
-            {
-                test: /\.json$/,
-                loader: 'json-loader'
-            }
-        ]
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            include: /\.min\.js$/,
-            minimize: true
-        })
-    ]
+  entry: {
+    index: ['babel-polyfill', './src/index.js'],
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    library: '[name]',
+    libraryTarget: 'commonjs2',
+    filename: '[name].js',
+  },
+  target: 'node',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+          plugins: ['syntax-flow', 'transform-flow-strip-types'],
+        },
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+    }),
+  ],
 };
