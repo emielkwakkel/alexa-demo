6a271477cadeaeb44bca9f47444a02ea91164918
Emiel Kwakkel <mail@emielkwakkel.nl>
2017-10-30 19:36:37 +0100

    💬 Improve conference open speech

--- src/intents/conference-open.intent.js
+++ src/intents/conference-open.intent.js
@@ -1,9 +1,16 @@
 export function conferenceOpen() {
   const speakersIntroduction = getSpeakersIntroduction();
-  const pronounceSogeti = 'I\'m far from replacing you Remco. I still have a hard time pronouncing Sogeti. Sogeti it is right?';
-  const speechOutput = `${pronounceSogeti}. Today we have a very interesting lineup arranged for you. ${speakersIntroduction}`;
+  const pronounceSogeti = 'I\'m far from replacing you <break time="0.3s" /> Remco. I still have a hard time pronouncing Sogeti. <break time="0.5s" /><phoneme alphabet="ipa" ph="sosheti">Sogeti</phoneme> it is right?';
+  const speechOutput = `
+    <p>The real question is: <break time="0.5s" /> Are <emphasis level="strong">you</emphasis> ready?</p><break time="1s" />
+    <p>${pronounceSogeti}</p>
+    <p>Today we have a very interesting lineup arranged for you.</p>
+    <p>${speakersIntroduction}<break time="0.5s" /></p>
+    <p>I hope you will all enjoy this evening!</p>`;
 
-  this.emit(':ask', speechOutput, repromtSpeech);
+  const repromptSpeech = 'Reprompting';
+
+  this.emit(':ask', speechOutput, repromptSpeech);
 }
 
 function getSpeakersIntroduction() {
@@ -13,7 +20,7 @@ function getSpeakersIntroduction() {
       talk: 'Introduce you to developing a skill for Amazon Alexa.',
     },
     {
-      name: 'Chris den Arend',
+      name: 'Chris',
       talk: 'Show you the Microsoft Bot Framework.',
     },
   ];
@@ -21,7 +28,7 @@ function getSpeakersIntroduction() {
   let speakersIntroduction = 'Let me anounce to you: ';
 
   speakers.forEach((speaker) => {
-    speakersIntroduction = `${speakersIntroduction} ${speaker.name} is going to ${speaker.talk} `;
+    speakersIntroduction = `${speakersIntroduction} <p><emphasis level="strong">${speaker.name}</emphasis> is going to ${speaker.talk}!</p><break time="0.5s" />`;
   });
 
   return speakersIntroduction;
