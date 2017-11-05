06cd4d1f953ff836ff8d1aeb915c885e4afb62d2
Emiel Kwakkel <mail@emielkwakkel.nl>
2017-11-05 22:32:04 +0100

    🤐 Silence that noisy eslinter

--- src/common/delegate-slot-collection.js
+++ src/common/delegate-slot-collection.js
@@ -1,15 +1,15 @@
-export function delegateSlotCollection() {
+export function delegateSlotCollection() { // eslint-disable-line consistent-return
   if (this.event.request.dialogState === 'STARTED') {
-    console.log('Dialog started');
+    console.log('Dialog started'); // eslint-disable-line no-console
 
     const updatedIntent = this.event.request.intent;
     this.emit(':delegate', updatedIntent);
   } else if (this.event.request.dialogState !== 'COMPLETED') {
-    console.log('Dialog in progress');
+    console.log('Dialog in progress'); // eslint-disable-line no-console
 
     this.emit(':delegate');
   } else {
-    console.log('Dialog completed');
+    console.log('Dialog completed'); // eslint-disable-line no-console
 
     return this.event.request.intent;
   }
