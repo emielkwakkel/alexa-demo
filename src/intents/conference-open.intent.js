4d588641b8087373c1f90894ec8e02abfc087564
Emiel Kwakkel <mail@emielkwakkel.nl>
2017-11-05 22:30:32 +0100

    🗣 Separate introduction using slots.

--- src/intents/conference-open.intent.js
+++ src/intents/conference-open.intent.js
@@ -2,17 +2,32 @@ export function conferenceOpen() {
   const speakersPartOne = getSpeakersIntroduction(1);
   const speakersPartTwo = getSpeakersIntroduction(2);
   const pronounceSogeti = 'I\'m far from replacing you <break time="0.3s" /> Remco. I still have a hard time pronouncing Sogeti. <break time="0.5s" /><phoneme alphabet="ipa" ph="sosheti">Sogeti</phoneme><break time="0.2s" /> it is right?';
-  const speechOutput = `
-    <p>The real question is: <break time="0.3s" /> Are <emphasis level="strong">you</emphasis> ready?</p><break time="1s" />
-    <p>${pronounceSogeti}</p>
+  const introduction = `
+    <p>The real question is: <break time="0.3s" /> Are <emphasis level="strong">you</emphasis> ready?</p>
+    <p>${pronounceSogeti}</p>`;
+  const speechPartOne = `
     <p>Today we have a very interesting lineup arranged for you.</p>
-    <p>'Let me anounce to you: '${speakersPartOne}<break time="0.5s" /></p>
+    <p>'Let me anounce to you: '${speakersPartOne}<break time="0.5s" /></p>`;
+  const speechPartTwo = `
     <p>'After the break: '${speakersPartTwo}<break time="0.5s" /></p>
     <p>I hope you will all enjoy this evening!</p>`;
 
-  const repromptSpeech = 'Reprompting';
+  const repromptSpeech = 'Could you say that again?';
+  const updatedIntent = this.event.request.intent;
 
-  this.emit(':ask', speechOutput, repromptSpeech);
+  console.log('updatedIntent', updatedIntent); // eslint-disable-line no-console
+
+  if (updatedIntent.slots.BeforeAfter && updatedIntent.slots.BeforeAfter.value === 'before') {
+    this.emit(':ask', speechPartOne, repromptSpeech);
+    updatedIntent.slots.BeforeAfter.value = null;
+  } else if (updatedIntent.slots.BeforeAfter && updatedIntent.slots.BeforeAfter.value === 'after') {
+    this.emit(':ask', speechPartTwo, repromptSpeech);
+    updatedIntent.slots.BeforeAfter.value = null;
+  } else {
+    this.emit(':ask', introduction, repromptSpeech);
+  }
+
+  this.emit(':delegate', updatedIntent);
 }
 
 function getSpeakersIntroduction(part) {
@@ -39,7 +54,7 @@ function getSpeakersIntroduction(part) {
     },
     {
       name: 'Thijs',
-      talk: 'tell you about Digital Happiness! From Clippy to the first AI-Human romance! I\'m sure it won\'t happen to you, but in near future people will get into a relationship with a robot or AI. Which trends can we spot in the use of chatbots and how do they connect to the start of a emotional economy',
+      talk: 'tell you about Digital Happiness! From Clippy to the first AI-Human romance! I\'m sure it won\'t happen to you, but in near future people will get into a relationship with a robot or AI. Which trends can we spot in the use of chatbots and how do they connect to the start of an emotional economy',
       part: 2,
     },
   ];
